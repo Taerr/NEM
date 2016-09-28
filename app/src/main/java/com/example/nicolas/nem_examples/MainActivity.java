8e60890519782a7d79ef2b5941535bc956ec0396
Taerr <nicolas.adam8@gmail.com>
2016-09-28 14:21:37 +0200

    example 4 - modifs textview

--- app/src/main/java/com/example/nicolas/nem_examples/MainActivity.java
+++ app/src/main/java/com/example/nicolas/nem_examples/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.nicolas.nem_examples;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,7 +19,12 @@ public class MainActivity extends AppCompatActivity {
         //Log.e("MainActivity", "This is an error message");
 
         //example 3
-        Toast.makeText(this, "This is a long toast message", Toast.LENGTH_LONG).show();
-        Toast.makeText(this, "This is a short toast message", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "This is a long toast message", Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "This is a short toast message", Toast.LENGTH_SHORT).show();
+
+        //example 4
+        TextView tv = (TextView) findViewById(R.id.textview);
+        tv.setText("This was changed from inside MainActivity");
+        tv.setTextSize(20);
     }
 }
