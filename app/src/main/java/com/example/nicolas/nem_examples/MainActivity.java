d2404695d7920dbf4d481f9ec849bbbd95005c44
Taerr <nicolas.adam8@gmail.com>
2016-09-28 14:00:44 +0200

    example 2 - log

--- app/src/main/java/com/example/nicolas/nem_examples/MainActivity.java
+++ app/src/main/java/com/example/nicolas/nem_examples/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.nicolas.nem_examples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("MainActivity", "This is an information message");
+        Log.wtf("MainActivity", "This is a WTF/Assertion Failure message.");
+        Log.e("MainActivity", "This is an error message");
+
     }
 }
