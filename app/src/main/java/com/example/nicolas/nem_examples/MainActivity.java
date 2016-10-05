29847e37fae2280417416251b9f96b3e1e5d0592
Taerr <nicolas.adam8@gmail.com>
2016-10-05 14:59:43 +0200

    example 18 - EditText

--- app/src/main/java/com/example/nicolas/nem_examples/MainActivity.java
+++ app/src/main/java/com/example/nicolas/nem_examples/MainActivity.java
@@ -1,26 +1,19 @@
 package com.example.nicolas.nem_examples;
 
-import android.content.res.Resources;
-import android.content.res.TypedArray;
-import android.support.annotation.IdRes;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import org.w3c.dom.Text;
 
-import static android.R.attr.onClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int clicks;
     private TextView tv_display;
-    private Button btn_clickme;
+    private EditText et_input;
 
 
 
@@ -29,22 +22,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_clickme = (Button) findViewById(R.id.btn_clickme);
-        btn_clickme.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0) {
-                clicks++;
-                tv_display.setText("Button has been clicked " + clicks + " times.");
+        tv_display = (TextView) findViewById(R.id.tv_display);
+        et_input = (EditText) findViewById(R.id.et_input);
+
+        et_input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE){
+                    tv_display.setText("Entered: " + et_input.getText());
+                    return true;
+                }
+                return false;
             }
         });
-
-        clicks = 0;
-        tv_display = (TextView) findViewById(R.id.tv_display);
     }
 
-    //public void buttonPress (View arg0){
-    //    clicks ++;
-    //    tv_display.setText("Button has been clicked " + clicks + " times.");
-    //}
 }
 
 
